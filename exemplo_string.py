081a70ca61136ad86dc65d4247beca7e4ba5bd7c
Mateus Rocha Pinto <mateusrpinto44@gmail.com>
2022-11-04 22:36:02 -0300

    exemplo manipulação de strings

---
+++ exemplo_string.py
@@ -0,0 +1,37 @@
+"amora"
+
+type("")
+
+"15" * str(2)
+
+"Legal!" * 3
+
+time = "Ibis"
+
+timr.upper()
+
+time.lower()
+
+"Mateus gosta de banana".capitalize()
+
+"BRAZIL".capitalize()
+
+"       mateusrpinto44@gmail.com      ".strip()
+
+"Eu torço para o Santos".replace("Santos", "Palmeiras")
+
+"Mateus gosta de laranja".capitalize().center()
+
+testo = "Minha terra tem palmeiras onde gorgeia o mar"
+
+x = "teoria"
+y = "prática"
+
+x == y
+x != y
+
+x > y
+
+ord("a")
+
+ord("A")
